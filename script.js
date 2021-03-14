eab47dd419382b6879dc629e7924d28633e1523b
Oguzhanolgun <oguzhanolgun35.5@gmail.com>
2021-03-14 21:49:33 +0300

    added playin variable and init function

--- script.js
+++ script.js
@@ -11,17 +11,14 @@ const $score0el = document.getElementById('score--0');
 const $score1el = document.getElementById('score--1');
 const $player0el = document.querySelector('.player--0');
 const $player1el = document.querySelector('.player--1');
+const $current0el = document.getElementById('current--0');
+const $current1el = document.getElementById('current--1');
 const $diceImg = document.querySelector('.dice');
 const $btnRoll = document.querySelector('.btn--roll');
 const $btnHold = document.querySelector('.btn--hold');
+const $btnNemGame = document.querySelector('.btn--new');
 
-$score0el.textContent = 0;
-$score1el.textContent = 0;
-$diceImg.classList.add('hidden');
-
-const scores = [0, 0];
-let activePlayer = 0;
-let currentScore = 0;
+let playing, scores, activePlayer, currentScore;
 
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -31,37 +28,58 @@ const switchPlayer = () => {
   $player1el.classList.toggle('player--active');
 };
 
+const init = () => {
+  playing = true;
+  scores = [0, 0];
+  activePlayer = 0;
+  currentScore = 0;
+  $score0el.textContent = 0;
+  $score1el.textContent = 0;
+  $current0el.textContent = 0;
+  $current1el.textContent = 0;
+  $diceImg.classList.add('hidden');
+  $player0el.classList.remove('player--winner');
+  $player1el.classList.remove('player--winner');
+  $player0el.classList.add('player--active');
+  $player1el.classList.remove('player--active');
+};
+init();
+
 $btnRoll.addEventListener('click', function () {
-  const randomDice = Math.trunc(Math.random() * 6 + 1);
-  console.log(randomDice);
-  if (randomDice > 1) {
-    $diceImg.src = `./images/dice-${randomDice}.png`;
-    $diceImg.classList.remove('hidden');
-    currentScore += randomDice;
-    document.getElementById(
-      `current--${activePlayer}`
-    ).textContent = currentScore;
-  } else {
-    // Switch Player
-    switchPlayer();
+  if (playing) {
+    const randomDice = Math.trunc(Math.random() * 6 + 1);
+    if (randomDice > 1) {
+      $diceImg.src = `./images/dice-${randomDice}.png`;
+      $diceImg.classList.remove('hidden');
+      currentScore += randomDice;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
+    } else {
+      // Switch Player
+      switchPlayer();
+    }
   }
 });
 
 $btnHold.addEventListener('click', function () {
-  scores[activePlayer] += currentScore;
-  console.log('currentScore', currentScore);
-  console.log('score[activePlayer]', scores[activePlayer]);
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
-  if (scores[activePlayer] >= 20) {
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--winner');
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player--active');
-    $diceImg.style.display = 'hidden';
-  } else {
-    switchPlayer();
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      $diceImg.classList.add('hidden');
+    } else {
+      switchPlayer();
+    }
   }
 });
+
+$btnNemGame.addEventListener('click', init);
